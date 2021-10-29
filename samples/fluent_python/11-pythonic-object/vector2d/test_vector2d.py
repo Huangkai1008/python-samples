4a1aa08514294c12209447502c84b64e4a6996f8
huangkai <h1770360848@outlook.com>
2021-10-29 22:54:29 +0800

    feat(fluent python): vector2d

---
+++ samples/fluent_python/11-pythonic-object/vector2d/test_vector2d.py
@@ -0,0 +1,56 @@
+import pytest
+
+from .vector2d import Vector2d
+
+
+class TestVector2d:
+    def test_vector2d_can_access_attributes_directly(self) -> None:
+        v = Vector2d(3, 4)
+
+        assert v.x == 3
+        assert v.y == 4
+
+    def test_vector2d_can_be_unpacked_to_a_tuple(self) -> None:
+        v = Vector2d(3, 4)
+
+        x, y = v
+
+        assert x == 3
+        assert y == 4
+
+    def test_vector2d_can_rebuild_from_repr(self) -> None:
+        v = Vector2d(3, 4)
+
+        v_clone = eval(repr(v))
+
+        assert v == v_clone
+
+    def test_vector2d_prints_as_tuple(self) -> None:
+        v = Vector2d(3, 4)
+
+        assert str(v) == '(3, 4)'
+
+    def test_vector_can_rebuild_from_bytes(self) -> None:
+        v = Vector2d(3, 4)
+
+        v_clone = Vector2d.from_bytes(bytes(v))
+
+        assert v == v_clone
+
+    def test_vector_abs_returns_magnitude(self) -> None:
+        v = Vector2d(3, 4)
+
+        assert abs(v) == 5.0
+
+    @pytest.mark.parametrize(
+        'v1,v2,expected',
+        [(3, 4, True), (0, 0, False)],
+    )
+    def test_vector2d_bool_returns_whether_magnitude_is_not_zero(
+        self, v1: float, v2: float, expected: bool
+    ) -> None:
+        v = Vector2d(v1, v2)
+
+        actual = bool(v)
+
+        assert actual == expected
---
+++ samples/fluent_python/11-pythonic-object/vector2d/vector2d.py
@@ -0,0 +1,41 @@
+import math
+from array import array
+from typing import Generator
+
+
+class Vector2d:
+    typecode = 'd'
+
+    def __init__(self, x: float, y: float) -> None:
+        self.x: float = x
+        self.y: float = y
+
+    @classmethod
+    def from_bytes(cls, octets: bytes) -> 'Vector2d':
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(*memv)
+
+    def __iter__(self) -> Generator[float, None, None]:
+        yield self.x
+        yield self.y
+
+    def __repr__(self) -> str:
+        return '{}({!r}, {!r})'.format(type(self).__name__, *self)
+
+    def __str__(self) -> str:
+        return str(tuple(self))
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Vector2d):
+            return tuple(self) == tuple(other)
+        return NotImplemented
+
+    def __bytes__(self) -> bytes:
+        return bytes([ord(self.typecode)]) + bytes(array(self.typecode, self))
+
+    def __abs__(self) -> float:
+        return math.hypot(self.x, self.y)
+
+    def __bool__(self) -> bool:
+        return bool(abs(self))
