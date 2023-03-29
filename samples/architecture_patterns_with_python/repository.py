1fa2446aa0dc9b6824d9372345bd608244446068
huangkai <h1770360848@outlook.com>
2023-03-29 14:54:41 +0800

    feat(architecture-patterns-with-python): repository pattern

---
+++ samples/architecture_patterns_with_python/repository.py
@@ -0,0 +1,34 @@
+from abc import ABCMeta, abstractmethod
+from typing import List, Optional
+
+from sqlalchemy.orm import Session
+
+from samples.architecture_patterns_with_python.model import Batch
+
+
+class AbstractRepository(metaclass=ABCMeta):
+    @abstractmethod
+    def add(self, batch: Batch) -> None:
+        ...
+
+    @abstractmethod
+    def get(self, reference: str) -> Optional[Batch]:
+        ...
+
+    @abstractmethod
+    def list(self) -> List[Batch]:
+        ...
+
+
+class SQLAlchemyRepository(AbstractRepository):
+    def __init__(self, session: Session) -> None:
+        self.session: Session = session
+
+    def add(self, batch: Batch) -> None:
+        self.session.add(batch)
+
+    def get(self, reference: str) -> Optional[Batch]:
+        return self.session.query(Batch).filter_by(reference=reference).first()
+
+    def list(self) -> List[Batch]:
+        return self.session.query(Batch).all()
